32c0f7211cdd762303d40a2410d3ef2f358bfd09
MaryuriL <90122369+MaryuriL@users.noreply.github.com>
2021-09-05 15:37:49 -0700

    results

--- Reto 4/mantenimiento.sql
+++ Reto 4/mantenimiento.sql
@@ -1,23 +1,18 @@
-CREATE TABLE `ejecutar` (
-	idarea INT NOT NULL, 
-	idmantenimiento INT NOT NULL,
-    FOREIGN KEY (idmantenimiento)
-        REFERENCES mantenimiento(idmantenimiento),
-    FOREIGN KEY (idarea)
-        REFERENCES area(idarea)
-);
-INSERT INTO ejecutar (idmantenimiento, idarea) VALUES ('1', '2');
-INSERT INTO ejecutar (idmantenimiento, idarea) VALUES ('1', '2');  
-INSERT INTO ejecutar (idmantenimiento, idarea) VALUES ('10', '4');                              
-INSERT INTO ejecutar (idmantenimiento, idarea) VALUES ('9', '3');                          
-INSERT INTO ejecutar (idmantenimiento, idarea) VALUES ('7', '1');                                       
-INSERT INTO ejecutar (idmantenimiento, idarea) VALUES ('7', '4');                                   
-INSERT INTO ejecutar (idmantenimiento, idarea) VALUES ('6', '1');               
-INSERT INTO ejecutar (idmantenimiento, idarea) VALUES ('4', '4');                                        
-INSERT INTO ejecutar (idmantenimiento, idarea) VALUES ('4', '2');                           
-INSERT INTO ejecutar (idmantenimiento, idarea) VALUES ('2', '1'); -- error                                              
-INSERT INTO ejecutar (idmantenimiento, idarea) VALUES ('2', '4');                                                             
-INSERT INTO ejecutar (idmantenimiento, idarea) VALUES ('8', '2');                                         
-INSERT INTO ejecutar (idmantenimiento, idarea) VALUES ('3', '4');                   
-INSERT INTO ejecutar (idmantenimiento, idarea) VALUES ('5', '1');          
-INSERT INTO ejecutar (idmantenimiento, idarea) VALUES ('6', '3');
+CREATE TABLE `mantenimiento` (
+	idmantenimiento INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	nombre VARCHAR(60) NOT NULL,
+	minutosbase INT NOT NULL,
+    idtipo INT NOT NULL, 
+	FOREIGN KEY (idtipo)
+        REFERENCES tipo(idtipo)
+  );
+INSERT INTO mantenimiento (nombre, minutosbase, idtipo) VALUES ('Ajustes de maquinaria y equipos', '120', '1');
+INSERT INTO mantenimiento (nombre, minutosbase, idtipo) VALUES ('Limpieza','130','2'); -- error
+INSERT INTO mantenimiento (nombre, minutosbase, idtipo) VALUES ('Análisis de las averías', '90','2');
+INSERT INTO mantenimiento (nombre, minutosbase, idtipo) VALUES ('Cambio de piezas', '240','2');
+INSERT INTO mantenimiento (nombre, minutosbase, idtipo) VALUES ('Reparación de equipos', '90','3');
+INSERT INTO mantenimiento (nombre, minutosbase, idtipo) VALUES ('Instalación componentes', '200','3');
+INSERT INTO mantenimiento (nombre, minutosbase, idtipo) VALUES ('Desmonte bandas', '400','2'); -- error
+INSERT INTO mantenimiento (nombre, minutosbase, idtipo) VALUES ('Calibración máquinas', '180','4');
+INSERT INTO mantenimiento (nombre, minutosbase, idtipo) VALUES ('Montaje eléctrico', '160','4');
+INSERT INTO mantenimiento (nombre, minutosbase, idtipo) VALUES ('Visita onPremise', '320','4');
